2e2aa626595c186712daad8c5d4c3396e2d994fd
MaWencan <wencanm@163.com>
2023-03-28 20:12:37 +0800

    master6(版本8)

--- src/main/java/com/itheima/it.java
+++ src/main/java/com/itheima/it.java
@@ -6,5 +6,6 @@ public class it {
         System.out.println("这是版本2-master2");
         System.out.println("这是版本3-master3");
         System.out.println("这是版本5-master4");
+        System.out.println("这是版本8-master5，感觉自己释怀了");
     }
 }
