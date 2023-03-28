78c47f6cd32585acf3d69bcf3fb544385de2f3a5
MaWencan <wencanm@163.com>
2023-03-28 20:11:06 +0800

    hotfix3(版本7)

--- src/main/java/com/itheima/it.java
+++ src/main/java/com/itheima/it.java
@@ -7,5 +7,6 @@ public class it {
         System.out.println("这是版本3-master3");
         System.out.println("这是版本4-hotfix1");
         System.out.println("这是版本6-hotfix2");
+        System.out.println("这是版本7-hotfix3,产生合并冲突");
     }
 }
