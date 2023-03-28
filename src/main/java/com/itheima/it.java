ae40b7d8d4dd96b701ad2ab7c722ce8816a41224
MaWencan <wencanm@163.com>
2023-03-28 20:02:38 +0800

    master2(版本2)

---
+++ src/main/java/com/itheima/it.java
@@ -0,0 +1,8 @@
+package com.itheima;
+
+public class it {
+    public static void main(String[] args) {
+        System.out.println("这是版本1-master1");
+        System.out.println("这是版本2-master2");
+    }
+}
